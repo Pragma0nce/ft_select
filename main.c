a4fc8b72da924ab71291ecd95d7303968d29e209
Kobus COETZEE <kcoetzee@e4r11p6.wethinkcode.co.za>
2017-11-21 11:29:36 +0200

    Implements non-canon mode

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <termios.h>
 #include "libft/libft.h"
+#include "get_next_line.h"
 
 // tgetnum
 // tgetflag
@@ -40,8 +41,12 @@ void	move_cursor(int hpos, int vpos)
 void	set_terminal_raw()
 {
 	struct termios options;
+
+	tcgetattr(STDIN_FILENO, &options);
+	options.c_lflag &= ~(ICANON|ECHO); /* Clear ICANON and ECHO. */
 	options.c_cc[VTIME] = 0;
 	options.c_cc[VMIN] = 4;
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &options);
 }
 
 void	init()
@@ -50,8 +55,15 @@ void	init()
 	set_terminal_raw();
 }
 
+void reset_input_mode(struct termios *saved)
+{
+	tcsetattr(STDIN_FILENO, TCSANOW, saved);
+}
+
 int main()
 {
+	struct termios saved;
+
 	init();
 
 	ft_putstr("ayy");
@@ -60,5 +72,13 @@ int main()
 	ft_putstr("boo");
 	move_cursor(10, 11);
 	ft_putstr("boo");
+
+	char *s;
+  	while ((s = get_next_line(0)))
+    {
+      my_putstr(s);
+      my_putchar('\n');
+      free(s);
+    }
 	return (0);
 }
